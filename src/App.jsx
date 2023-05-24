b87edc04d8ef5ccbcd1b2e94bad9064f7496d110
Vlad Donchak <vlad.donchak1996@gmail.com>
2023-05-24 20:51:40 +0300

    route settings and initial layout of the home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,51 @@ import { useState, useEffect } from 'react'
 import { fetchDataFromApi } from './utils/api'
 import { useSelector, useDispatch } from 'react-redux'
 import { getApiConfiguration } from './store/homeSlice'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import Home from './pages/home/Home'
+import Details from './pages/details/Details'
+import SearchResult from './pages/searchResult/SearchResult'
+import Explore from './pages/explore/Explore'
+import PageNotFound from './pages/error_page/pageNotFound'
 
 function App() {
   const { url } = useSelector((state) => state.home)
   const dispatch = useDispatch()
 
   useEffect(() => {
-    apiTest()
+    fetchApiConfig()
   }, [])
 
-  const apiTest = () => {
-    fetchDataFromApi('/movie/popular').then((response) => {
-      console.log(response)
-      dispatch(getApiConfiguration(response))
+  const fetchApiConfig = () => {
+    fetchDataFromApi('/configuration').then((res) => {
+      console.log(res)
+
+      const url = {
+        backdrop: res.images.secure_base_url + 'original',
+        poster: res.images.secure_base_url + 'original',
+        profile: res.images.secure_base_url + 'original',
+      }
+
+      dispatch(getApiConfiguration(url))
     })
   }
 
-  return <div className="App">App {url?.total_pages}</div>
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
+  )
 }
 
 export default App
