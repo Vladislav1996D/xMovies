21012795de82340585e8180b5e545bd365cab601
Vlad Donchak <vlad.donchak1996@gmail.com>
2023-05-31 23:54:28 +0300

    added Details Page (Details Banner Section)

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -45,7 +45,7 @@ const HeroBanner = () => {
 
       <ContentWrapper>
         <div className="heroBannerContent">
-          <span className="title">Welcome.</span>
+          <span className="title">Welcome</span>
           <span className="subTitle">
             Millions of movies, TV shows and people to discover. Explore now.
           </span>
