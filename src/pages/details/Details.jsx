3c2aba572dac50b8bd8bc2b5353da52da204099f
Vlad Donchak <vlad.donchak1996@gmail.com>
2023-06-01 21:50:16 +0300

    added Top Cast, Videos, Similar & 'Tips' sections

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-import useFetch from '../../hooks/useFetch'
 import { useParams } from 'react-router-dom'
+import useFetch from '../../hooks/useFetch'
 import DetailsBanner from './detailsBanner/DetailsBanner'
+import Cast from './cast/Cast'
+import VideoSection from './videoSection/VideoSection'
+import Similar from './carousels/Similar'
+import Recommendation from './carousels/Recommendation'
 
 import './style.scss'
 
@@ -15,6 +19,10 @@ const Details = () => {
   return (
     <div>
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideoSection data={data} loading={loading} />
+      <Similar mediaType={mediaType} id={id} />
+      <Recommendation mediaType={mediaType} id={id} />
     </div>
   )
 }
